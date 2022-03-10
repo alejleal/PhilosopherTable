090087114dde9d25a25f482ecc424748ac10a48c
alejleal <alejleal@ucm.es>
2022-03-10 10:56:37 +0100

    Archivos iniciales

---
+++ monitor.py
@@ -0,0 +1,18 @@
+class Table:
+    def __init__(self, nphil, manager):
+        pass
+
+    def set_current_phil(self, pid):
+        pass
+
+    def wants_eat(self, pid):
+        pass
+
+    def wants_think(self, pid):
+        pass
+
+class CheatMonitor:
+    pass
+
+class AnticheatTable:
+    pass
